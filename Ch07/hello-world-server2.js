ff1aef62ee3f411ec143f884316793af5d200bc3
jt01052552295 <jt01052552295@gmail.com>
2016-02-28 19:44:06 +0900

    test

---
+++ Ch07/hello-world-server2.js
@@ -0,0 +1,11 @@
+var connect = require('connect')
+var http = require('http');
+var app = connect();
+
+app.use(function(req, res){
+	
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World \n');
+    
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
